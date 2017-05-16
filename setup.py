6ddc4c320c48073c1e8bf2ca5106f6e7a4675460
IgnacioYanjari <nach116@hotmail.com>
2017-05-16 16:35:29 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ sql = """
 CREATE TABLE Negocios( id serial PRIMARY KEY , Dueno_id integer , Ubicacion varchar , Comuna varchar , Gastos integer);
 CREATE TABLE Stocks(id serial PRIMARY KEY , Negocio_id integer , Producto_id integer , Stock_producto integer , Proveedor_id integer , Precio integer );
 CREATE TABLE Dueños(id serial PRIMARY KEY , Nombre varchar , Telefono integer , Email varchar );
-CREATE TABLE Proveedores(id serial PRIMARY KEY , Stock_id integer , Telefono integer , Ubicacion varchar ,Stock integer);
+CREATE TABLE Proveedores(id serial PRIMARY KEY , Stock_id integer , Telefono integer , Ubicacion varchar ,Precio integer);
 CREATE TABLE Productos(id serial PRIMARY KEY , Nombre varchar , Detalle varchar);
-CREATE TABLE Ventas(id serial PRIMARY KEY , Negocio_id integer, Monto integer , Num_venta integer , Fecha varchar , Producto_id integer);
-"""
+CREATE TABLE Ventas(num_venta serial PRIMARY KEY , Negocio_id integer, Fecha datetime);
+CREATE TABLE Ventas_detalle(num_venta integer , Producto_id integer , Monto integer , Cantidad integer);
 
+"""
+#queda con 255 el varchar
 cur.execute(sql)
 conn.commit()
 cur.close()
