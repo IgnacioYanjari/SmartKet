b832152c956190bfbdc4de7699b325ba8e8b39e1
IgnacioYanjari <nach116@hotmail.com>
2017-05-18 15:29:34 -0400

    Upload setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ sql = """
 CREATE TABLE Negocios( id serial PRIMARY KEY , Dueno_id integer , Calle varchar , Comuna varchar, Ciudad varchar, Ragion varchar, Telefono integer);
 CREATE TABLE Stocks(Negocio_id serial PRIMARY KEY ,  Producto_id integer , Stock_producto integer , Proveedor_id integer , Precio integer );
 CREATE TABLE Duenos(id serial PRIMARY KEY , Nombre varchar , Telefono integer , Email varchar );
-CREATE TABLE Proveedores(id serial PRIMARY KEY , Telefono integer ,Comuna varchar,Ciudad varchar ,Region varchar ,Calle varchar ,Precio integer,Nombre varchar ,Prod_detalle varchar );
+CREATE TABLE Proveedores(id serial PRIMARY KEY , Telefono integer ,Comuna varchar,Ciudad varchar ,Region varchar ,Calle varchar ,Precio integer,Nombre varchar);
 CREATE TABLE Productos(id serial PRIMARY KEY , Nombre varchar , Detalle varchar);
 CREATE TABLE Ventas(Num_venta serial PRIMARY KEY , Negocio_id integer, Fecha datetime, Total integer);
 CREATE TABLE Ventas_detalle(Num_venta serial PRIMARY KEY , Producto_id integer , Monto integer , Cantidad integer);
