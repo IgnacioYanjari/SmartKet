efec00e9292745ae97e4e39d7dd4f4153a6f6b54
Ignacio Yanjari <nach116@hotmail.com>
2017-05-26 14:03:40 -0400

    seed.py upload(2)

--- app/seed.py
+++ app/seed.py
@@ -1,42 +1,45 @@
-from config import * 
+from config import *
 import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,password))
 cur = conn.cursor()
 
 sql = """
-insert into duenos (id,nombre, telefono, email) values ('0','dueno_test', '0', 'mail_test') 
-returning 
+insert into duenos (nombre, telefono, email) values ('dueno_test', '0', 'mail_test@test.cl')
+returning
 id, nombre, telefono, email;
 
-insert into Negocios (id, dueno_id, calle, comuna, ciudad, region, telefono) values 
-('0','0','calle_test','comuna_test','ciudad_test','region_test','0') returning 
+insert into Negocios (dueno_id, calle, comuna, ciudad, region, telefono) values
+('0','calle_test','comuna_test','ciudad_test','region_test','1')
+returning
 id, dueno_id, calle, comuna, ciudad, region, telefono;
 
-insert into stocks (negocio_id,producto_id, stock_producto, proveedor_id, precio) values 
-('0','0', '0','0','0') 
-returning 
-negocio_id,producto_id, stock_producto, proveedor_id, precio;
+insert into stocks (negocio_id,producto_id, stock_producto, proveedor_id, precio) values
+('0','0', '10','0','1000')
+returning negocio_id,producto_id, stock_producto, proveedor_id, precio;
 
-insert into proveedor (id,telefono, comuna, ciudad, region, calle, precio, nombre) values ('0','0', 'comuna_test', 'ciudad_test','region_test','calle_test','0','nombre_test') 
-returning 
+insert into proveedor (telefono, comuna, ciudad, region, calle, precio, nombre)
+values ('2', 'comuna_test', 'ciudad_test','region_test','calle_test','800','nombre_test')
+returning
 id,telefono, comuna, ciudad, region, calle, precio, nombre;
 
-insert into productos (id,nombre, detalle) values ('0','nombre_test','detalle_test') 
-returning 
+insert into productos (nombre, detalle) values ('nombre_test','detalle_test')
+returning
 id,nombre, detalle;
 
-insert into ventas (num_venta, negocio_id, fecha) values ('0','0', '0/0/0') 
-returning 
+insert into ventas (num_venta, negocio_id, fecha) values ('0','0', '0/0/0')
+returning
 num_venta, negocio_id, fecha;
 
-insert into ventas_detalle (num_venta, producto_id, monto, cantidad) values ('0','0', '0', '0') 
-returning 
+insert into ventas_detalle (num_venta, producto_id, monto, cantidad)
+values ('0','0', '1000', '3')
+returning
 num_venta, producto_id, monto, cantidad;
+
 """
 # Revisar returning
 cur.execute(sql)
 conn.commit()
-dueno = cur.fetchone()
+dueno = cur.fetchall()
 print dueno
 
 conn.close()
