e8ac9173e2e0f0af5c04ff633a8add0b3386d129
Dago31 <dagoa7x@gmail.com>
2017-05-25 21:30:40 -0400

    Update seed.py

--- seed.py
+++ seed.py
@@ -4,8 +4,34 @@ conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,password)
 cur = conn.cursor()
 
 sql = """
-insert into duenos (nombre, telefono, email) values ('Dagoberto Navarrete', '911', 'thudaghitobienbellako@realg4life.cl') returning 
+insert into duenos (id,nombre, telefono, email) values ('0','dueno_test', '0', 'mail_test') 
+returning 
 id, nombre, telefono, email;
+
+insert into Negocios (id, dueno_id, calle, comuna, ciudad, region, telefono) values 
+('0','0','calle_test','comuna_test','ciudad_test','region_test','0') returning 
+id, dueno_id, calle, comuna, ciudad, region, telefono;
+
+insert into stocks (negocio_id,producto_id, stock_producto, proveedor_id, precio) values 
+('0','0', '0','0','0') 
+returning 
+negocio_id,producto_id, stock_producto, proveedor_id, precio;
+
+insert into proveedor (id,telefono, comuna, ciudad, region, calle, precio, nombre) values ('0','0', 'comuna_test', 'ciudad_test','region_test','calle_test','0','nombre_test') 
+returning 
+id,telefono, comuna, ciudad, region, calle, precio, nombre;
+
+insert into productos (id,nombre, detalle) values ('0','nombre_test','detalle_test') 
+returning 
+id,nombre, detalle;
+
+insert into ventas (num_venta, negocio_id, fecha) values ('0','0', '0/0/0') 
+returning 
+num_venta, negocio_id, fecha;
+
+insert into ventas_detalle (num_venta, producto_id, monto, cantidad) values ('0','0', '0', '0') 
+returning 
+num_venta, producto_id, monto, cantidad;
 """
 # Revisar returning
 cur.execute(sql)
