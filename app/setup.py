e129cf76ee0246e2e232c6d67bb525d8c05487f0
Ignacio Yanjari <nach116@hotmail.com>
2017-05-28 00:06:07 -0400

    fix setup.py

--- app/setup.py
+++ app/setup.py
@@ -11,12 +11,12 @@ cur.execute(sql)
 
 sql = """
 CREATE TABLE negocios( id serial PRIMARY KEY , dueno_id integer , calle varchar , comuna varchar, ciudad varchar, region varchar, telefono integer);
-CREATE TABLE stocks(Negocio_id serial PRIMARY KEY ,  producto_id integer , stock_producto integer , proveedor_id integer , precio integer );
+CREATE TABLE stocks(Negocio_id integer ,  producto_id integer , stock_producto integer , proveedor_id integer , precio integer );
 CREATE TABLE duenos(id serial PRIMARY KEY , nombre varchar , telefono integer , email varchar );
 CREATE TABLE proveedores(id serial PRIMARY KEY , telefono integer ,comuna varchar,ciudad varchar ,region varchar ,calle varchar ,precio integer,nombre varchar);
 CREATE TABLE productos(id serial PRIMARY KEY , nombre varchar , detalle varchar);
-CREATE TABLE ventas(num_venta serial PRIMARY KEY , negocio_id integer, fecha timestamp, total integer);
-CREATE TABLE ventas_detalle(num_venta serial PRIMARY KEY , producto_id integer , monto integer , cantidad integer);
+CREATE TABLE ventas(num_venta integer , negocio_id integer, fecha timestamp);
+CREATE TABLE ventas_detalle(num_venta integer , producto_id integer , monto integer , cantidad integer);
 """
 #queda con 255 el varchar
 cur.execute(sql)
