c605c1d5f428114139b1a40fbb00d842c13ffa6b
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2017-06-15 23:34:41 -0400

    Falta cambiar la seed y una parte del views.py, la consulta de la wea de sacar telefono y comuna

--- app/views.py
+++ app/views.py
@@ -9,4 +9,20 @@ cur = conn.cursor()
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    sql = """
+        select nombre from duenos where id = '1';
+    """
+    print sql
+    cur.execute(sql)
+    duenos = cur.fetchone()
+
+    sql = """
+
+        select negocios.telefono , negocios.calle
+        from negocios  where  negocios.id='1';
+    """
+    print sql
+    cur.execute(sql)
+    datos = cur.fetchone()
+    print datos
+    return render_template("index.html",duenos=duenos , datos = datos)
