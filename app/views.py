be3601e721e644c7c84bd6e11984bbbc3878e975
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2017-06-16 00:21:18 -0400

    Fecha

--- app/views.py
+++ app/views.py
@@ -24,5 +24,16 @@ def index():
     print sql
     cur.execute(sql)
     datos = cur.fetchone()
-    print datos
-    return render_template("index.html",duenos=duenos , datos = datos)
+#current_data , current_time or EXTRACT(querido from donde)
+    sql = """
+        select ventas_detalle.num_venta, t1.suma, ventas.fecha
+        from ventas,ventas_detalle,
+        (select ventas_detalle.num_venta,sum(monto) as suma  from ventas_detalle group by num_venta) as t1
+        where t1.num_venta = ventas_detalle.num_venta and ventas.num_venta = ventas_detalle.num_venta
+    """
+    print sql
+    cur.execute(sql)
+    ventas = cur.fetchall()
+    print ventas
+
+    return render_template("index.html",duenos=duenos , datos = datos , ventas = ventas)
