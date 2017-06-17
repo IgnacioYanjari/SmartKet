306082a176ac993b3f2a5b221481dd27ecb49603
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2017-06-17 12:01:01 -0400

    fecha

--- app/views.py
+++ app/views.py
@@ -26,21 +26,24 @@ def index():
     datos = cur.fetchone()
 
     sql = """
-        select ventas_detalle.num_venta, t1.suma, ventas.fecha
-        from ventas,ventas_detalle,
-        (select ventas_detalle.num_venta,sum(monto) as suma  from ventas_detalle group by num_venta) as t1
-        where t1.num_venta = ventas_detalle.num_venta and ventas.num_venta = ventas_detalle.num_venta
+        select ventas_detalle.num_venta, t1.suma, ventas.fecha from ventas,ventas_detalle,(select ventas_detalle.num_venta,sum(monto) as suma  from ventas_detalle group by num_venta) as t1 where t1.num_venta = ventas_detalle.num_venta and ventas.num_venta = ventas_detalle.num_venta
     """
     print sql
     cur.execute(sql)
     ventas = cur.fetchall()
-    for subventa in ventas
+    tupla = []
+    for subventa in ventas:
+        tupla2 = []
+        for subsubventa in subventa:
+            tupla2.append(subsubventa)
+        tupla.append(tupla2)
+    for subventa in tupla:
+        #print (subventa[2].hour)
+        fechas = str(subventa[2].day)+"/"+str(subventa[2].month)+"/"+str(subventa[2].year)
+        horas = str(subventa[2].hour)+":"+str(subventa[2].minute)+":"+str(subventa[2].second)
+        print(fechas,horas)
+        subventa.append(fechas)
+        subventa.append(horas)
 
-        fecha = str(ventas[2].day)+"/"+str(ventas[2].month)+"/"+str(ventas[2].year)
-        hora = str(ventas[2].hour)+":"+str(ventas[2].minute)+":"str(ventas[2].second)
-        ventas[2] = fecha
-        ventas.append(hora)
-
-    print ventas
-
-    return render_template("index.html",duenos=duenos , datos = datos , ventas = ventas)
+    print tupla
+    return render_template("index.html",duenos=duenos , datos = datos , ventas = tupla)
